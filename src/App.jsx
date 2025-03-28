08c06701df849713abd95e10e8103fbc518b2f99
Prado Brian Nicolas <nicolas88prado@gmail.com>
2025-03-28 09:20:32 -0300

    agregue el maquetado necesario para el componente Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import Footer from './components/common/Footer'
 
 function App() {
 
   return (
     <>
       <h1>Hola </h1>
+      <Footer></Footer>
     </>
   )
 }
--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import './footer.css'
+//import logo from '../../../assets/Logo.png'
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { Col, Row } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='bgFooter text-center'>
+      <NavLink to={'/'}>
+      {/* <img src={logo} alt="" width={250} className='imgLogo' /> */}
+      </NavLink>
+      <div className='d-md-flex d-lg-flex gap-3 justify-content-center pb-4'>
+        <NavLink className='text-white linksFooter' to={"/"}><p >Home</p></NavLink>
+       <NavLink className='text-white linksFooter'> <p >Nosotros</p></NavLink>
+        <NavLink className='text-white linksFooter'><p >Productos</p></NavLink>
+        <NavLink to={'/Login'} className='text-white linksFooter'><p >Login</p></NavLink>
+      </div>
+      <div className='pb-2 d-flex gap-4 justify-content-center'>
+        <p className='text-white btnRedondo'><i class="bi bi-instagram"></i></p>
+        <p className='text-white btnRedondo'><i class="bi bi-facebook"></i></p>
+        <p className='text-white btnRedondo'><i class="bi bi-twitter"></i></p>
+      </div>
     </div>
   )
 }
